55183ad057027ed61ea465dafef4aa0be8bfa83d
heloise-g <heloise.gaspais@iseg.fr>
2020-06-30 17:29:41 +0200

    travail j6
    continuation

--- EX_03/boucles-simples.php
+++ EX_03/boucles-simples.php
@@ -1,10 +1,15 @@
 <?php
+    $maximum = 20
+?>
+<?php
     function compter($maximum){
-        $maximum = "50";
-        $index = "1";
-        while ($index < $maximum){
-            echo $index;
-            $index + $index + 1;
+        $boucle1 = 0;
+        while ($boucle1 < $maximum){
+            echo $boucle1;
+            $boucle1 = $boucle1 + 1;
         }
     }
+?>
+<?php
+compter($maximum);
 ?>
