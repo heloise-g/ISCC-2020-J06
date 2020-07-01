ca16d92fbc3963a7146b1e8cb850304b0fa13d8b
heloise-g <heloise.gaspais@iseg.fr>
2020-07-01 14:19:41 +0200

    travail mercredi
    finit tous les exercices

--- EX_03/boucles-imbriquees.php
+++ EX_03/boucles-imbriquees.php
@@ -1,7 +1,7 @@
 <?php
     function clock(){
         for($h = 0; $h <= 24; $h = $h + 1){
-        for($m=0, $m <= 59; $m = $m + 1){
+        for($m=0, $m <= 59; $m = $m + 1) {
             echo $m . "<br/>";
         }
         }
